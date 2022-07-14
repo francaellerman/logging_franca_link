08ee3936db2ab423b3d3b9e2c198dea1a53d10c1
Franca Ellerman <franca@castedo.com>
2022-07-14 17:04:57 -0400

    That's embarassing

---
+++ README.md
@@ -0,0 +1,2 @@
+# logging_franca_link
+A little package with logging stuff so I don't have circular imports in my multiple packages for franca.link
---
+++ logging_franca_link/__init__.py
@@ -0,0 +1,85 @@
+import logging
+import flask
+
+#Simplified from https://stackoverflow.com/a/70223539/11141301
+class JsonFormatter(logging.Formatter):
+    """
+    Formatter that outputs JSON strings after parsing the LogRecord.
+    """
+    def formatMessage(self, record) -> dict:
+        return record.__dict__
+
+    def format(self, record) -> str:
+        if record.levelname == 'WARNING':
+            warnings.warn('There was a logging warning')
+        record.message = record.getMessage()
+        #Should be allowed to not specify format, see https://docs.python.org/3/library/logging.html#logging.Formatter.formatTime
+        record.asctime = self.formatTime(record)
+        message_dict = self.formatMessage(record)
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            message_dict["exc_info"] = record.exc_text
+        if record.stack_info:
+            message_dict["stack_info"] = self.formatStack(record.stack_info)
+        return json.dumps(message_dict, default=str)
+
+def set_up_logging:
+    global JsonFormatter
+    logging.captureWarnings(True)
+    #No parameters to getLogger returns root logger
+    root = logging.getLogger()
+    root.setLevel(level=logging.DEBUG)
+    class connections_filter(logging.Filter):
+        def filter(self, record):
+            return (record.name == 'connections' or record.levelno >= 30) and record.msg.find('Using fallback font') == -1 and not record.extra['ignore']
+    handler = logging.handlers.RotatingFileHandler('franca_link.log', maxBytes=10**6, backupCount=5)
+    formatter = JsonFormatter()
+    handler.setFormatter(formatter)
+    handler.addFilter(connections_filter())
+    root.addHandler(handler)
+    with open('/etc/franca_link/email_address.txt') as f:
+        from_ = f.readline().rstrip()
+        to = f.readline().rstrip()
+    email_handler = logging.handlers.SMTPHandler(mailhost='localhost',
+            fromaddr = from_, toaddrs=[to], subject='Connections log')
+    email_handler.addFilter(connections_filter())
+    #root.addHandler(email_handler)
+
+class wrapper_related:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+
+    def extra(self, id_=None):
+        ip = flask.request.environ.get('HTTP_X_REAL_IP',
+                flask.request.remote_addr)
+        student_id = id_ if id_ else flask.session.get('ID')
+        return {'IP': ip, 'ID': student_id,
+                'ignore': flask.session.get('ignore'),
+                'flask_path': flaks.request.path,
+                'flask_method': flask.request.method}
+
+    def info(self, message, id_=None):
+        self.logger.info(message, extra=extra(id_))
+
+    def exception(self, id_=None):
+        self.logger.exception("Runtime exception", extra=self.extra(id_))
+
+    def wrapper(self, message=None):
+        def inner(func):
+            def inner_inner():
+                try:
+                    resp = func()
+                    if not message:
+                        message = 'Request success'
+                    self.info(message)
+                    return resp
+                except:
+                    self.exception()
+                    return flask.abort(500)
+            inner_inner.__name__ = func.__name__
+            return inner_inner
+        return inner
